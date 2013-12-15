ee16abb3699ec8f364c7c5730b698a8a209f6890
Dmitry Sorin <info@staypositive.ru>
2013-12-15 15:47:51 +0400

    Updated content script w/ support for audio

--- src/content_scripts/external.js
+++ src/content_scripts/external.js
@@ -5,11 +5,16 @@
     var LISTEN_APP_ID = "nhpdcfndackfenecoefcmphfjhdnaedi";
     var AD_URL = "14300_286";
     var LEFT_MENU_ID = "l_listen_app";
+    var LEFT_MENU_LAUNCH_ID = "l_listen_app_launch";
     var LEFT_MENU_COUNTER_ID = "left_listen_app_cnt";
     var DOM_PREVENT_ELEM_ID = "prevent_listen_tmp";
+    var DOM_ELEM_AUDIO_RESULT_CHOICE = "tmp_audio_choice";
+    var DOM_ELEM_LAUNCH = "tmp_launch_app";
     var STORAGE_KEY_PREVENT = "prevent_listen_app_ad";
+    var STORAGE_KEY_PREVENT_AUDIO = "prevent_listen_app_audio";
     var STORAGE_KEY_SEEN = "prevent_listen_app_seen";
     var WIKIBOX_AD_ID = "listen_app_wikiad";
+    var AUDIOBOX_AD_ID = "listen_app_audioad";
     var TIMEOUT_DEBOUNCE_MS = 500;
 
     var debounceTimeoutId;
@@ -18,6 +23,10 @@
 
     // mutation observer
     function mutatoDebounce() {
+        processPreventAudio();
+        processPreventDOMItem();
+        processLaunchApp();
+
         if (debounceTimeoutId)
             window.clearTimeout(debounceTimeoutId);
 
@@ -30,7 +39,6 @@
             return;
 
         processWikiBox();
-        processPreventDOMItem();
 
         chrome.runtime.sendMessage(LISTEN_APP_ID, {action: "isAlive"}, function (isAlive) {
             var menuItemLeft = document.getElementById(LEFT_MENU_ID);
@@ -40,7 +48,8 @@
                     menuItemLeft.parentNode.removeChild(menuItemLeft);
                 }
 
-                // в зависимости от URL показать ad
+                processAudioSearch();
+                appendLaunchMenuItem();
             } else {
                 var keys = {};
                 keys[STORAGE_KEY_SEEN] = false;
@@ -60,7 +69,10 @@
     }
 
     function appendMenuItem(withCnt) {
-        console.log("Insert menu element");
+        var menuItemLeftLauncher = document.getElementById(LEFT_MENU_LAUNCH_ID);
+        if (menuItemLeftLauncher) {
+            menuItemLeftLauncher.parentNode.removeChild(menuItemLeftLauncher);
+        }
 
         var menuSettingsItem = document.getElementById("l_set");
         var menuItemLeft = document.getElementById(LEFT_MENU_ID);
@@ -76,11 +88,15 @@
 
         if (menuItemLeft) {
             if (withCnt && !menuItemLeftCnt) {
+                console.log("Update menu element");
                 menuItemLeft.firstChild.insertAdjacentHTML("afterbegin", counterElemHTML);
             } else if (!withCnt && menuItemLeftCnt) {
+                console.log("Update menu element");
                 menuItemLeftCnt.parentNode.removeChild(menuItemLeftCnt);
             }
         } else {
+            console.log("Insert menu element");
+
             var menuElementTitle = withCnt ? "Моя Реклама" : "Приложение Listen!";
 
             var menuElementHTML = [
@@ -94,6 +110,32 @@
 
             menuSettingsItem.insertAdjacentHTML("afterend", menuElementHTML);
         }
+    }
+
+    function appendLaunchMenuItem() {
+        var menuItemLeftSimple = document.getElementById(LEFT_MENU_ID);
+        if (menuItemLeftSimple) {
+            menuItemLeftSimple.parentNode.removeChild(menuItemLeftSimple);
+        }
+
+        var menuItemLeft = document.getElementById(LEFT_MENU_LAUNCH_ID);
+        if (menuItemLeft)
+            return;
+
+        console.log("Insert launch menu element");
+
+        var menuElementTitle = (!/^en/i.test(navigator.language)) ? "Приложение Listen!" : "Listen! app";
+
+        var menuElementHTML = [
+            '<li id="' + LEFT_MENU_LAUNCH_ID + '">',
+                '<a href="/wall-' + AD_URL + '" onclick="el = document.createElement(\'span\'); el.id = \'' + DOM_ELEM_LAUNCH + '\'; document.body.appendChild(el); return false;" class="left_row">',
+                    '<span class="left_label inl_bl">' + menuElementTitle + '</span>',
+                '</a>',
+            '</li>'
+        ].join("");
+
+        var menuSettingsItem = document.getElementById("l_set");
+        menuSettingsItem.insertAdjacentHTML("afterend", menuElementHTML);
     }
 
     function processWikiBox() {
@@ -130,6 +172,129 @@
         chrome.storage.sync.set(records);
 
         preventElem.parentNode.removeChild(preventElem);
+    }
+
+    function parseSearch() {
+        var searchParts = {};
+        location.search.substr(1).split("&").forEach(function (part) {
+            if (!part)
+                return;
+
+            part = part.split("=", 2);
+            searchParts[part[0]] = decodeURIComponent(part[1]);
+        });
+
+        var audioSearchInput = document.getElementById("s_search");
+        if (audioSearchInput) {
+            searchParts.q = audioSearchInput.value;
+        }
+
+        return searchParts;
+    }
+
+    function processAudioSearch() {
+        if (location.pathname !== "/audio")
+            return;
+
+        chrome.storage.sync.get(STORAGE_KEY_PREVENT_AUDIO, function (records) {
+            var searchParts = parseSearch();
+            var audioBox = document.getElementById(AUDIOBOX_AD_ID);
+
+            if (records[STORAGE_KEY_PREVENT_AUDIO]) {
+                if (audioBox) {
+                    audioBox.parentNode.removeChild(audioBox);
+                }
+
+                return;
+            }
+
+            if (audioBox) {
+                if (audioBox.dataset.search === JSON.stringify(searchParts))
+                    return;
+
+                audioBox.parentNode.removeChild(audioBox);
+            }
+
+            console.log("Insert audio box");
+
+            var openPlayer = (!/^en/i.test(navigator.language)) ? "Открыть Listen!" : "Launch Listen!";
+            var noThanks = (!/^en/i.test(navigator.language)) ? "Спасибо, не надо" : "No, thanks";
+            var textAd;
+
+            if (!searchParts.q) {
+                textAd = (!/^en/i.test(navigator.language))
+                    ? "Слушать свои песни гораздо удобнее в облачном плеере Listen! для Google Chrome. А LastFM подберет вам близких по звучанию исполнителей."
+                    : "You can listen to your own music collection in Listen! app for Google Chrome. LastFM service will also find the most similar artists for you.";
+            } else if (searchParts.performer == 1) {
+                textAd = (!/^en/i.test(navigator.language))
+                    ? "Вы можете найти самые популярные песни и список альбомов <b>" + searchParts.q + "</b> в облачном плеере Listen! для Google Chrome"
+                    : "You can listen to the most popular songs and see full discography of <b>" + searchParts.q + "</b> in Listen! app for Google Chrome";
+            } else {
+                textAd = (!/^en/i.test(navigator.language))
+                    ? "Искать музыку гораздо удобнее в облачном плеере Listen! для Google Chrome. Попробуйте поискать <b>" + searchParts.q + "</b> там и почувствуйте разницу!"
+                    : "Search for <b>" + searchParts.q + "</b> is really smarter in Listen! app for Google Chrome. Check this out and feel the difference!";
+            }
+
+            var externalBoxStyle = "text-align: left; margin-bottom: 0; padding-left: 13px; padding-right: 13px;";
+            var onClickActionRemoveBox = "elem = document.getElementById('" + AUDIOBOX_AD_ID + "'); elem.parentNode.removeChild(elem);";
+            var onClickAction = "newNode = document.createElement('span'); newNode.id = '" + DOM_ELEM_AUDIO_RESULT_CHOICE + "'; newNode.dataset.choice = '%choice%'; document.body.appendChild(newNode);";
+
+            var advBlockHTML = [
+                '<div style="' + externalBoxStyle + '" class="left_box" id="' + AUDIOBOX_AD_ID + '">',
+                    textAd,
+                    '<div style="margin-top: 8px">',
+                        '<div class="button_blue" style="margin-right: 8px">',
+                            '<button type="button" onclick="' + onClickAction.replace("%choice%", "1") + ' return false;">' + openPlayer + '</button>',
+                        '</div>',
+                        '<div class="button_gray">',
+                            '<button type="button" onclick="' + onClickAction.replace("%choice%", "0") + onClickActionRemoveBox + ' return false;">' + noThanks + '</button>',
+                        '</div>',
+                    '</div>',
+                '</div>'
+            ].join('');
+
+            var searchBox = document.getElementById("audio_search");
+            searchBox.insertAdjacentHTML("afterend", advBlockHTML);
+
+            document.getElementById(AUDIOBOX_AD_ID).dataset.search = JSON.stringify(searchParts);
+        });
+    }
+
+    function processPreventAudio() {
+        var preventElem = document.getElementById(DOM_ELEM_AUDIO_RESULT_CHOICE);
+        if (!preventElem)
+            return;
+
+        var searchParts = parseSearch();
+        var launchListen = (preventElem.dataset.choice == 1);
+        var msgData;
+
+        if (launchListen) {
+            if (!searchParts.q) {
+                msgData = {action: "getCurrent"};
+            } else if (searchParts.performer == 1) {
+                msgData = {action: "searchArtist", q: searchParts.q};
+            } else {
+                msgData = {action: "searchSongs", q: searchParts.q};
+            }
+
+            chrome.runtime.sendMessage(LISTEN_APP_ID, msgData);
+        } else {
+            var records = {};
+            records[STORAGE_KEY_PREVENT_AUDIO] = true;
+            chrome.storage.sync.set(records);
+        }
+
+        preventElem.parentNode.removeChild(preventElem);
+    }
+
+    function processLaunchApp() {
+        var tmpElem = document.getElementById(DOM_ELEM_LAUNCH);
+        if (!tmpElem)
+            return;
+
+        chrome.runtime.sendMessage(LISTEN_APP_ID, {action: "getCurrent"});
+        tmpElem.parentNode.removeChild(tmpElem);
     }
 
     var observer = new (window.MutationObserver || window.WebKitMutationObserver)(mutatoDebounce);
